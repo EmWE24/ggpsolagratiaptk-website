620ef8dbf0041610e7b8e15f2b8f88e6dc2ee476
EmWE24 <jimmy12tkj@gmail.com>
2024-03-17 09:39:26 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -15,16 +15,29 @@ function changeBackground() {
 
 setInterval(changeBackground, 5000);
 
-const Navbar = document.querySelector(".nav-navbar");
+const NavbarNav = document.querySelector(".nav-navbar");
 document.querySelector("#Menu").onclick = () => {
-  Navbar.classList.toggle("active");
+  NavbarNav.classList.toggle("active");
 };
 
 const Menu = document.querySelector("#Menu");
 document.addEventListener(
   (onclick = function (e) {
-    if (!Menu.contains(e.target) && !Navbar.contains(e.target)) {
-      Navbar.classList.remove("active");
+    if (!Menu.contains(e.target) && !NavbarNav.contains(e.target)) {
+      NavbarNav.classList.remove("active");
     }
   })
 );
+
+const NavBar = document.querySelector(".navbar");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (window.scrollY > 20) {
+    NavBar.classList.remove("transparent");
+  } else {
+    NavBar.classList.add("transparent");
+  }
+}
